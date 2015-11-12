e02461ffc14a8e61e8d4ec21a8599b03be120292
yelpspoon <djc@34thplace.net>
2015-11-12 00:32:24 -0600

    Programming Assignment #2 for Scope

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+# `makeCacheMatrix`: This function creates a special "matrix" object
+#  that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(gma = matrix()) {
+    inv_ma <- NULL
+    set <- function(ma) {
+      gma    <<- ma
+      inv_ma <<- NULL
+    }
+    get <- function() gma
+    setinv <- function(invma) inv_ma <<- invma
+    getinv <- function() inv_ma
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# `cacheSolve`: This function computes the inverse of the special
+#  "matrix" returned by `makeCacheMatrix` above. If the inverse has
+#  already been calculated (and the matrix has not changed), then
+#  `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv_ma <- x$getinv()
+    if(!is.null(inv_ma)) {
+      message("getting cached data")
+      return(inv_ma)
+    }
+    data <- x$get()
+    inv_ma <- solve(data, ...)
+    x$setinv(inv_ma)
+    inv_ma
 }
+
+## test data
+#my.data <- rbind(c(3, 5), c(7, 9))
+#my.ma <- makeCacheMatrix(my.data)
+#cacheSolve(my.ma)
